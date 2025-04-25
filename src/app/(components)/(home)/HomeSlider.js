96e61a4d270b361996ae14fa26d80bbaf703cabb
Rusith Dhyan <rusithdhyan2000@gmail.com>
2025-04-25 08:50:42 +0530

    change home slider

--- src/app/(components)/(home)/HomeSlider.js
+++ src/app/(components)/(home)/HomeSlider.js
@@ -37,18 +37,18 @@ export default function HomeSlider() {
       >
         {homeSlider.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div className="flex flex-col sm:flex-row  items-center justify-center gap-5">
-              <div className="relative">
-                <Image
-                  src={slide.url} // Ensure the images exist
-                  alt="slide-img"
-                  width={1500}
-                  height={100}
-                  className="max-h-screen h-screen object-cover w-full"
-                />
-              </div>
-              <div className="absolute inset-0 bottom-0 flex items-end justify-center pb-5">
-                <h1 className="absolute inset-0 flex items-end justify-center text-2xl md:text-3xl lg:text-5xl text-white pb-4 font-bold px-4">
+            <div className="relative w-full">
+              <Image
+                src={slide.url}
+                alt="slide-img"
+                layout="responsive"
+                width={1500}
+                height={600}
+                className="object-cover w-full max-h-screen h-screen"
+              />
+
+              <div className="absolute inset-0 flex items-end justify-center pb-5">
+                <h1 className="text-2xl md:text-3xl lg:text-5xl text-white font-bold px-4 text-center drop-shadow-md">
                   {slide.title}
                 </h1>
               </div>
