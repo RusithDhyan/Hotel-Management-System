0e816457a4a5900319803cff25ea7b04edb480f6
Rusith Dhyan <rusithdhyan2000@gmail.com>
2025-06-11 14:27:01 +0530

    resolve the font issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,49 @@
+// import "./globals.css";
+// import { Jost } from "next/font/google";
+// import Footer from "./(components)/Footer";
+// import Navbar from "./(components)/Navbar";
+// import { DataProvider } from "./context/DataContext";
+
+// const jost = Jost({
+//   subsets: ["latin"], 
+//   variable: "--font-jost",
+// });
+
+// export const metadata = {
+//   title: "Serendib Resort",
+//   description: "Hotel Management System",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <link rel="icon" href="/favicon.ico" />
+//       <body className={`${jost.variable} font-sans`}>
+//         <DataProvider>
+//         <div className="flex flex-col min-h-screen">
+//             <Navbar />
+//             <main className="flex-grow">{children}</main>
+//             <Footer />
+//           </div>
+//         </DataProvider>
+          
+//       </body>
+//     </html>
+//   );
+// }
+
+
 import "./globals.css";
-import { Jost } from "next/font/google";
 import Footer from "./(components)/Footer";
 import Navbar from "./(components)/Navbar";
 import { DataProvider } from "./context/DataContext";
+import localFont from 'next/font/local';
 
-// Correct configuration with subsets defined
-const jost = Jost({
-  subsets: ["latin"], // REQUIRED for avoiding preload error
-  variable: "--font-jost", // Optional, use if you're using Tailwind for variables
+
+const argueDemo = localFont({
+  src: '/fonts/ArgueDemo-Regular.ttf',
+  variable: '--font-argue', // optional for CSS variable use
+  display: 'swap', // improves rendering
 });
 
 export const metadata = {
@@ -17,9 +53,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={argueDemo.className}>
       <link rel="icon" href="/favicon.ico" />
-      <body className={`${jost.variable} font-sans`}>
+      <body>
         <DataProvider>
         <div className="flex flex-col min-h-screen">
             <Navbar />
